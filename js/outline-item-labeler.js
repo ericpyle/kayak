71243eab2c6c956b824c001969e65e2b0eb022cc
ericpyle <eric.d.pyle@gmail.com>
2013-08-22 20:08:01 -0500

    create as cons module.

--- js/outline-item-labeler.js
+++ js/outline-item-labeler.js
@@ -1,61 +1,85 @@
-﻿
-/*
-*
-*	var dto = {
-*				type: "chiasm",
-*				concepts: [
-*						{ content: "a1" },
-*						{ content: "a2", type: "seq" }
-*				]
-*			};
-*/
-
-function getLabel(dto, index) {
-	if (dto.type == "chiasm") {
-		var labels = getLabels(dto, index);
-		return labels[index];
-	}
-	return -1;
+﻿/*
+ * dto = { before: "", num: "", after: "" }
+ */
+function NumLabel(dto) {
+	if (!dto)
+		dto = { before: "", num: "", after: "" };
+	this.before = dto.before.toString();
+	this.num = dto.num.toString();
+	this.after = dto.after.toString();
 }
 
-function getLabels(dto, indexToStop) {
-	var labels = [];
-	if (!indexToStop)
-		indexToStop = dto.concepts.length - 1;
-	if (dto.type == "chiasm") {
-		var embedded = { parentEnum: "", parentEndMarker: "", total: 0, currentCount: 0 };
-		var mainlineIndex = -1;
-		for (var i = 0; i <= indexToStop; i++) {
-			var concept = dto.concepts[i];
-			var label = "";
-			if (!concept.embeddedType) {
-				mainlineIndex++;
-				var chr = IndexToAsciiMarkerABA(mainlineIndex, dto.concepts.length - embedded.total);
-				var endchar = GetEndMarkerABA(mainlineIndex, dto.concepts.length - embedded.total);
-				embedded.parentEndMarker = endchar;
-				embedded.parentEnum = chr;
-				embedded.currentCount = 0;
-				// look ahead to see if we need to add any special embedded enumeration
-				if (i + 1 < dto.concepts.length) {
-					var nextConcept = dto.concepts[i + 1];
-					if (nextConcept.embeddedType == "panel") {
+NumLabel.prototype.toString = function () {
+	return this.before + this.num + this.after;
+}
+
+/* cons - Custom Outline Numbering Services
+ */
+var cons = (function () {
+
+	var api = {};
+	api.getLabel = getLabel;
+	api.getLabels = getLabels;
+	return api;
+
+	/*
+	 *
+	 *	var dto = {
+	 *				type: "chiasm",
+	 *				concepts: [
+	 *						{ content: "a1" },
+	 *						{ content: "a2", type: "seq" }
+	 *				]
+	 *			};
+	 */
+	function getLabel(dto, index) {
+		if (dto.type == "chiasm") {
+			var labels = getLabels(dto, index);
+			return labels[index];
+		}
+		return -1;
+	}
+
+	function labelToString(label) {
+		return label.before + label.num + label.after;
+	}
+
+	function getLabels(dto, indexToStop) {
+		var labels = [];
+		if (!indexToStop)
+			indexToStop = dto.concepts.length - 1;
+		if (dto.type == "chiasm") {
+			var embedded = { parentEnum: "", parentEndMarker: "", total: 0, currentCount: 0 };
+			var mainlineIndex = -1;
+			for (var i = 0; i <= indexToStop; i++) {
+				var concept = dto.concepts[i];
+				var label = new NumLabel("", "", "");
+				if (!concept.embeddedType || concept.embeddedType == "panel" && concept.isHead) {
+					mainlineIndex++;
+					var chr = IndexToAsciiMarkerABA(mainlineIndex, dto.concepts.length - embedded.total);
+					var endchar = GetEndMarkerABA(mainlineIndex, dto.concepts.length - embedded.total);
+					embedded.parentEndMarker = endchar;
+					embedded.parentEnum = chr;
+					embedded.currentCount = 0;
+					if (concept.embeddedType == "panel" && concept.isHead) {
 						embedded.currentCount++;
 						embedded.total++;
 						chr = chr + "1";
-					}
+					}				
+					label.num = chr;
+					label.after = endchar;
 				}
-				label = chr + endchar;
+				else if (concept.embeddedType == "panel") {
+					embedded.currentCount++;
+					label.num = embedded.parentEnum + embedded.currentCount;
+					label.after = embedded.parentEndMarker;
+					embedded.total++;
+				}
+				labels.push(label);
 			}
-			else if (concept.embeddedType == "panel") {
-				embedded.currentCount++;
-				label = embedded.parentEnum + embedded.currentCount + embedded.parentEndMarker;
-				embedded.total++;
-			}
-			labels.push(label);
 		}
+		return labels;
 	}
-	return labels;
-}
 
 function GetEndMarkerABA(index, count) {
 	var halfway = Math.round(count / 2);
@@ -104,3 +128,4 @@ function GetEndMarkerAAB(index) {
 		endchar = "'";
 	return endchar;
 }
+})();
