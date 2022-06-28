dba01a6bad605a5733626677a8f4c424327f8b76
Eric Pyle <eric_pyle@sil.org>
2022-06-27 20:43:27 -0500

    fix backup to prompt to copy to outlineslive.json.bak

--- data/backup-data.bat
+++ data/backup-data.bat
@@ -1,2 +1,14 @@
+@echo off
+setlocal
+:PROMPT
+SET /P AREYOUSURE=Are you sure (Y/[N])?
+IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
+
+@echo on
+COPY outlineslive.json outlineslive.json.bak
 REM See https://stackoverflow.com/a/37294271
 curl -X GET http://localhost:59840/outlineslive/_all_docs?include_docs=true --header "Authorization: Basic YWRtaW46cGFzc3dvcmQ=" | jq "{"""docs""": [.rows[].doc]}" | jq "del(.docs[]._rev)" > outlineslive.json
+
+
+:END
+endlocal
--- data/restore-data.bat
+++ data/restore-data.bat
@@ -1,2 +1,1 @@
-COPY outlineslive.json outlineslive.json.bak
 curl --request PUT --url http://127.0.0.1:59840/outlineslive --header "Authorization: Basic YWRtaW46cGFzc3dvcmQ=" && curl --request POST --data-ascii "@outlineslive.json" --url http://127.0.0.1:59840/outlineslive/_bulk_docs --header "Authorization: Basic YWRtaW46cGFzc3dvcmQ=" --header "Content-type: application/json"
