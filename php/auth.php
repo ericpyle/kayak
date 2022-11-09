174e3e7f8ca66d42515367bf55b9579ea94b2307
Eric Pyle <eric_pyle@sil.org>
2022-11-08 21:43:36 -0600

    try pass options

--- php/auth.php
+++ php/auth.php
@@ -1,3 +1,47 @@
 <?php
-	echo 'Hello, World!';
+// FROM https://stackoverflow.com/a/9802854
+// Method: POST, PUT, GET etc
+// Data: array("param" => "value") ==> index.php?param=value
+
+$val = getopt("p:");
+if ($val !== false) {
+	echo var_export($val, true);
+}
+else {
+	echo "Could not get value of command line option\n";
+}
+
+function CallAPI($method, $url, $data = false)
+{
+    $curl = curl_init();
+
+    switch ($method)
+    {
+        case "POST":
+            curl_setopt($curl, CURLOPT_POST, 1);
+
+            if ($data)
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+            break;
+        case "PUT":
+            curl_setopt($curl, CURLOPT_PUT, 1);
+            break;
+        default:
+            if ($data)
+                $url = sprintf("%s?%s", $url, http_build_query($data));
+    }
+
+    // Optional Authentication:
+    curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+    curl_setopt($curl, CURLOPT_USERPWD, "username:password");
+
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+    $result = curl_exec($curl);
+
+    curl_close($curl);
+
+    return $result;
+}
 ?>
